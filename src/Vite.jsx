e38642139a681e30920e6d686c1b20a9729d4f3a
Kálóczi Kornél <kkornel.ctr@tenable.com>
2023-11-06 20:11:43 +0100

    fix things

--- src/Vite.jsx
+++ src/Vite.jsx
@@ -2,17 +2,19 @@ import {useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {useNavigate} from "react-router-dom";
 
 function Vite() {
     const [count, setCount] = useState(0)
+    const navigate = useNavigate();
 
     return (
         <>
             <div>
-                <a href="https://vitejs.dev" target="_blank">
+                <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
                     <img src={viteLogo} className="logo" alt="Vite logo"/>
                 </a>
-                <a href="https://react.dev" target="_blank">
+                <a href="https://react.dev" target="_blank" rel="noreferrer">
                     <img src={reactLogo} className="logo react" alt="React logo"/>
                 </a>
             </div>
@@ -28,6 +30,9 @@ function Vite() {
             <p className="read-the-docs">
                 Click on the Vite and React logos to learn more
             </p>
+            <div className="links">
+                <button onClick={()=> navigate('/demo')}>Go to the new page</button>
+            </div>
         </>
     )
 }
